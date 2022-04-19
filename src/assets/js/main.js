ef2e0182f613df6b63745cf9be5107656463627b
ambedo <ambedoweb@gmail.com>
2022-04-19 22:42:01 +0100

    readd js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,5 @@
-// @import './src/assets/js/anchor/anchor.js';
-// @import './src/assets/js/accordion/accordion.js';
-// @import './src/assets/js/carousel/carousel.js';
-// @import './src/assets/js/nav/navbtn.js';
-// @import './src/assets/js/nav/navbar.js';
+@import './src/assets/js/anchor/anchor.js';
+@import './src/assets/js/accordion/accordion.js';
+@import './src/assets/js/carousel/carousel.js';
+@import './src/assets/js/nav/navbtn.js';
+@import './src/assets/js/nav/navbar.js';
